5e52656faf0d37e57faf516d360de9aa3b5ae0a8
Hernan Espinoza <hernan.espinoza.dev@gmail.com>
2026-01-08 02:09:26 -0300

    chore: add sitemap and refine seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,16 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://hotusoft.com',
   base: '/',
+
   vite: {
     plugins: [/** @type {any} */ (tailwindcss())]
-  }
+  },
+
+  integrations: [sitemap()]
 });
